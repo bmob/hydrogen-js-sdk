e314bfb641e84d4b2fa8f7cb2550daee4345b910
lingyikeng <2210388316@qq.com>
2018-04-18 16:45:55 +0800

    修改生成小程序二维码，获取access_token，新增小程序模版信息SDK

--- src/lib/common.js
+++ src/lib/common.js
@@ -6,7 +6,7 @@ const Bmob = require('./bmob')
  */
 const generateCode = (data, options) => {
   let route = Bmob._config.parameters.GENERATECODE
-  return request(route,null,null,'get')
+  return request(route,'post',data)
 }
 /**
  * 获取access_token
@@ -14,7 +14,16 @@ const generateCode = (data, options) => {
  */
 const getAccessToken = (data, options) => {
   let route = Bmob._config.parameters.GETACCESSTOKEN
-  return request(route,null,null,'get')
+  return request(route,'get')
+}
+
+/**
+ * 小程序模版信息
+ * @return {Object}
+ */
+const sendWeAppMessage = (data, options) => {
+    let route = Bmob._config.parameters.SENDWEAPPMESSAGE
+    return request(route,'post',data)
 }
 
 const sendMessage = (data, options) => {
@@ -22,4 +31,4 @@ const sendMessage = (data, options) => {
   return 1
 }
 
-module.exports = {generateCode,sendMessage,getAccessToken};
+module.exports = {generateCode,sendMessage,getAccessToken,sendWeAppMessage};
