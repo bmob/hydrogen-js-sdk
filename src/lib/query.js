eb317a3821f097de14a8c6e20e8685f4edfb2b63
Yan <1176989241@qq.com>
2018-04-18 22:11:25 +0800

    实现更新单条数据

--- src/lib/query.js
+++ src/lib/query.js
@@ -2,12 +2,23 @@ const request = require('./request')
 const {isObject, isString} = require('./dataType')
 const query = class query {
   constructor(parma) {
-    this.tableName = parma
+    this.tableName = `/1/classes/${parma}`
     this.setData = {}
   }
   get(parma) {
+    let oneData = {}
+    const set = (key, val) => {
+      if (isString(key)) {
+        oneData[key] = val
+      }
+    }
+    const save = () => {
+      request(`${this.tableName}/${parma}`, 'put', oneData)
+    }
     return new Promise((resolve, reject) => {
-      request(`/1/classes/${this.tableName}/${parma}`).then(results => {
+      request(`${this.tableName}/${parma}`).then(results => {
+        results.__proto__.set = set
+        results.__proto__.save = save
         resolve(results)
       }).catch(err => {
         reject(err)
@@ -23,11 +34,11 @@ const query = class query {
     if (isObject(parma)) {
       this.setData = Object.assign(parma, this.setData)
     }
-    return request(`/1/classes/${this.tableName}`, 'post', this.setData)
+    return request(`${this.tableName}`, 'post', this.setData)
   }
   find() {
     return new Promise((resolve, reject) => {
-      request(`/1/classes/${this.tableName}`).then(({results}) => {
+      request(`${this.tableName}`).then(({results}) => {
         resolve(results)
       }).catch(err => {
         reject(err)
--- src/main.js
+++ src/main.js
@@ -5,60 +5,65 @@ import Bmob from './lib/app'
 console.log(Bmob)
 
 
-// Bmob.initialize("71acb3659ea66abed6b7739f9bd2e914","45ef983f011036c5868e9e9a38c193ec")
-Bmob.initialize("83c627d276f41bc0f62adbd5ce875e8a","b939b7e1b0c8b221dd4a1320dbc4cd4d")
+// Bmob.initialize("83c627d276f41bc0f62adbd5ce875e8a","b939b7e1b0c8b221dd4a1320dbc4cd4d")
 //
-// 
-// 生成二维码
-let qrData = { path: 'path', width: 430, type: 1 }
-Bmob.generateCode(qrData).then(function (response) {
-    console.log(response);
-})
-.catch(function (error) {
-    console.log(error);
-});
-
-
-// 获取access_token
-Bmob.getAccessToken().then(function (response) {
-    console.log(response);
-})
-.catch(function (error) {
-    console.log(error);
-});
-
-// 小程序模板消息
-let modelData = {
-    "touser": "open_Id",
-    "template_id": "template_id",
-    "page": "index",
-    "form_id":"form_Id",
-    "data": {
-        "keyword1": {
-            "value": "SDK测试内容",
-            "color": "#173177"
-        },
-        "keyword2": {
-            "value": "2018年04月18日 16:30"
-        },
-        "keyword3": {
-            "value": "Bmob科技"
-        }
-    }
-    ,"emphasis_keyword": ""
-}
-Bmob.sendWeAppMessage(modelData).then(function (response) {
-    console.log(response);
-})
-.catch(function (error) {
-    console.log(error);
-});
-// const query = Bmob.Query('project');
-// query.get('iET1NDDN').then(res => {
-//   console.log(res)
-// }).catch(err => {
-//   console.log(err)
+//
+// // 生成二维码
+// let qrData = { path: 'path', width: 430, type: 1 }
+// Bmob.generateCode(qrData).then(function (response) {
+//     console.log(response);
 // })
+// .catch(function (error) {
+//     console.log(error);
+// });
+//
+//
+// // 获取access_token
+// Bmob.getAccessToken().then(function (response) {
+//     console.log(response);
+// })
+// .catch(function (error) {
+//     console.log(error);
+// });
+//
+// // 小程序模板消息
+// let modelData = {
+//     "touser": "open_Id",
+//     "template_id": "template_id",
+//     "page": "index",
+//     "form_id":"form_Id",
+//     "data": {
+//         "keyword1": {
+//             "value": "SDK测试内容",
+//             "color": "#173177"
+//         },
+//         "keyword2": {
+//             "value": "2018年04月18日 16:30"
+//         },
+//         "keyword3": {
+//             "value": "Bmob科技"
+//         }
+//     }
+//     ,"emphasis_keyword": ""
+// }
+// Bmob.sendWeAppMessage(modelData).then(function (response) {
+//     console.log(response);
+// })
+// .catch(function (error) {
+//     console.log(error);
+// });
+
+Bmob.initialize("71acb3659ea66abed6b7739f9bd2e914","45ef983f011036c5868e9e9a38c193ec")
+
+const query = Bmob.Query('project');
+query.get('iET1NDDN').then(res => {
+  console.log(res)
+  res.set("name","修改的成功")
+  res.set("cover","1111")
+  res.save()
+}).catch(err => {
+  console.log(err)
+})
 
 
 // //
