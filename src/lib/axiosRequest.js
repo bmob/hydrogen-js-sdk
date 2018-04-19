47d1cd73ed0df119894df4d151b824a2a5003245
magic <ysjheeqg@163.com>
2018-04-19 16:06:45 +0800

    git status

--- src/lib/axiosRequest.js
+++ src/lib/axiosRequest.js
@@ -7,28 +7,92 @@ const utils = require('./utils')
 const setHeader = (config) => {
   let header = {
     'content-type': 'application/json',
+    'X-Bmob-SDK-Type': 'hybrid',
     'X-Bmob-Application-Id': config.applicationId,
     'X-Bmob-REST-API-Key': config.applicationKey,
   }
   return header
 }
 
-const request = (route, method = "get",parma = {}) => {
-    return new Promise((resolve, reject) => {
-      const header = setHeader(Bmob._config)
-      axios({
+const request = (route, method = "get", parma = {}) => {
+  return new Promise((resolve, reject) => {
+    const header = setHeader(Bmob._config)
+
+    // axios({
+    //   method: method,
+    //   url: `${Bmob._config.host}${route}`,
+    //   params: parma,
+    //   headers: header,
+    //   data: parma,
+    //   // `transformResponse` 在传递给 then/catch 前，允许修改响应数据
+    //   transformResponse: [function (data) {
+    //     // 对 data 进行任意转换处理
+    //     const obj = JSON.parse(data)
+    //     if (obj.code) {
+    //       reject(data);
+    //     }
+    //     return data;
+    //   }],
+
+    // }).then(({ data }) => {
+    //   resolve(data);
+    // }).catch(error => {
+    //   console.log(error);
+    //   reject(error);
+    // });
+
+
+
+    const server = axios.create({
+      baseURL: Bmob._config.host,
+      // baseURL: 'http://192.168.0.66:8088',
+      // timeout: 20000,
+      headers: header
+    })
+
+    // 添加响应拦截器
+    server.interceptors.response.use(response => {
+      console.log('jjj', response.data.code)
+      switch (response.data.code) {
+        case 0:
+          return response.data;
+          break;
+        case 100:
+          break;
+      }
+      return response.data
+    }, error => {
+      console.log("请求内容错误")
+      return Promise.reject(error,999);
+    });
+
+    if (method == 'get') {
+      server({
+        url: route,
         method: method,
-        url: `${Bmob._config.host}${route}`,
         params: parma,
-        headers: header,
-        data: parma
-      }).then(({data}) => {
-          resolve(data);
+      }).then(({ data }) => {
+        resolve(data);
+      }).catch(error => {
+        console.log(error,999);
+        reject(error);
+      });
+    } else {
+      server({
+        url: route,
+        method: method,
+        parma,
+
+      }).then(({ data }) => {
+        resolve(data);
       }).catch(error => {
         console.log(error);
         reject(error);
       });
     }
+
+
+  }
   );
 
 
