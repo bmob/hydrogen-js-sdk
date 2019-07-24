86d07d6b1d9b92dae8e9ed8b1f418ba4370975b4
magic <ysjheeqg@163.com>
2019-07-24 14:20:25 +0800

    fix bug

--- src/lib/bmob.js
+++ src/lib/bmob.js
@@ -5,6 +5,9 @@ Bmob.utils = utils
 Bmob._config = utils.getConfig()
 
 Bmob.initialize = (secretKey, securityCode, masterKey) => {
+  if (securityCode > 6) {
+    console.log(`Bmob初始化失败，2.0版本SDK请使用API安全码初始化，文档地址：https://bmob.github.io/hydrogen-js-sdk/#/?id=初始化`)
+  }
   Bmob._config.secretKey = secretKey
   Bmob._config.securityCode = securityCode
   Bmob._config.applicationMasterKey = masterKey
