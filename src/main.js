88f764e131584d48a38f330253702c9b3b288bad
magic <magic@ldeMacBook-Pro.local>
2024-05-31 15:07:52 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -15,36 +15,47 @@ var Bmob = require('./lib/app')
 // Bmob.initialize('4df53b03a0b3a8ef', '123456');
 // Bmob.initialize('ad1ef6c1eac9b6e7', '123456'); //内网
 
-Bmob.initialize('9731770784b8c006', '111111')
-Bmob.domain('http://website-restful.bmobapp.com')
+Bmob.initialize('042598233f386a87', 'googleabc') // 博士
+
+// Bmob.initialize('9731770784b8c006', '111111')
+// Bmob.domain('http://website-restful.bmobapp.com')
 Bmob.debug(true);
 
-// 引入sdk
-var Bmob = require('../dist/Bmob-2.5.1.min')
 
-// 初始化key
-Bmob.initialize('9731770784b8c006', '111111')
-
-// 初始化AI链接
-let ChatAi = Bmob.ChatAI()
-
-// 模拟3秒后按钮点击发送消息
-setTimeout(()=>{
-  // session 会话id，可以传用户objectId，或者随机数
-  let datas = {"messages":[{"content":"你好","role":"user"}],"session":"b1"}
-  ChatAi.send(JSON.stringify(datas))
-},3000)
-
-// 返回消息处理
-let msg = ''
-ChatAi.onMessage((res)=>{
-  if(res=="done"){
-    // 打印消息
-    console.log(msg);
-  }else{
-    msg = msg+res
-  }
+const query = Bmob.Query('tests');
+query.get('40d4364d96').then(res => {
+  console.log(res)
+}).catch(err => {
+  console.log(err)
 })
+console.log("aaa");
+
+// // 引入sdk
+// var Bmob = require('../dist/Bmob-2.5.1.min')
+
+// // 初始化key
+// Bmob.initialize('9731770784b8c006', '111111')
+
+// // 初始化AI链接
+// let ChatAi = Bmob.ChatAI()
+
+// // 模拟3秒后按钮点击发送消息
+// setTimeout(()=>{
+//   // session 会话id，可以传用户objectId，或者随机数
+//   let datas = {"messages":[{"content":"你好","role":"user"}],"session":"b1"}
+//   ChatAi.send(JSON.stringify(datas))
+// },3000)
+
+// // 返回消息处理
+// let msg = ''
+// ChatAi.onMessage((res)=>{
+//   if(res=="done"){
+//     // 打印消息
+//     console.log(msg);
+//   }else{
+//     msg = msg+res
+//   }
+// })
 
 
 // const query = Bmob.Query('_User')
